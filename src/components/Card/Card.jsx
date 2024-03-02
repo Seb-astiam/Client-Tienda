806c6f644cb723fc9510e22820db6954ccb99295
GIrigoin <guilleirigoin@hotmail.com>
2024-03-02 17:40:22 -0300

    instalando tailwind

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Card = ({ product }) => {
   //console.log("first", product);
-  const { id, name, price, brand, type, color, genre, image, Stocksizes } = product;
+  const { id, name, price, brand, type, color, genre, image, Stocksizes } =
+    product;
   return (
-    <div>
-    <img src={image}></img>
-      <h1>{name}</h1>
+    <div className="bg-red-400 rounded border-8 border-yellow-400">
+      <img src={image}></img>
+      <h1 className="text-3xl font-bold underline text-center border-r-emerald-600">
+        {name}
+      </h1>
       <h2>{brand}</h2>
       <h2>{genre}</h2>
       <h3>${price}</h3>
