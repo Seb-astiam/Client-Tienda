e90e25b31231e7c78c766759a292a4df74e7fa86
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-06 13:43:28 -0500

    Estilo de cards responsive Sebastian

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,7 +4,7 @@ import Card from "../Card/Card";
 const Cards = ({ arrProducts }) => {
   // console.log("pro", props);
   return (
-    <div>
+    <div className="grid 2xl:grid-cols-6 xl:grid-cols-5  lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-2 justify-items-center">
       {arrProducts.map((product, index) => (
         <Card product={product} key={index} />
       ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
+
+
   theme: {
+    colors:{
+      'black':'#020617',      
+      'yellow':'#DEFF10',
+      'white':'#fafaf9',
+    },
     extend: {},
   },
   plugins: [],
